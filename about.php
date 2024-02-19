2df38c3dd92adada7ad432299498263d92eb07f4
natsteven <nathanaelsteven@gmail.com>
2024-02-18 22:14:48 -0700

    some styling

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <?php require_once "header.php"; ?>
 <div class="container">
-    <h1>About PartyGames.social</h1>
+    <h2>About PartyGames.social</h2>
     <p>Welcome to PartyGames.social, the ultimate destination for online party games! Whether you're hosting a virtual party or just looking for some fun games to play with friends, we've got you covered.</p>
     <p>Our platform offers a wide variety of interactive games that can be enjoyed by players of all ages. From trivia quizzes to charades, there's something for everyone.</p>
     <p>Join our community today and start having a blast with your friends and family. Let the games begin!</p>
