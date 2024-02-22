f93d107a1fc2aa39aa66016edfaeebd32f06aaf2
natsteven <nathanaelsteven@gmail.com>
2024-02-21 17:47:15 -0700

    dobbedo

--- header.php
+++ header.php
@@ -4,10 +4,22 @@
     <meta charset="UTF-8">
     <title>PartyGames</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
 <body>
     <header>
-        <h1 id="banner">PartyGames!</h1>
+        <!-- <a href="index.php"> -->
+        <div id="banner">
+            <h1><a href="index.php">PartyGames!</a></h1>
+            <div class="login">
+                <a href="login.php">Login</a>
+            </div>
+        </div>
+
+        <!-- </a> -->
         <nav>
             <ol>
                 <li><a href="index.php">Home</a></li>
