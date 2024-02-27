4a17b1fed9c2f3c70f0cd2a6912dd80fdf77187c
natsteven <nathanaelsteven@gmail.com>
2024-02-26 17:28:41 -0700

    initial db stuff

--- init.sql
+++ init.sql
@@ -17,6 +17,23 @@ CREATE TABLE IF NOT EXISTS user_preferences (
     FOREIGN KEY(preference_id) REFERENCES preferences(id)
 );
 
+CREATE TABLE game_sessions (
+    room_code TEXT PRIMARY KEY,
+    host_user_id INTEGER,
+    host_guest_id INTEGER,
+    game_state TEXT,
+    FOREIGN KEY(host_user_id) REFERENCES users(id)
+);
+
+CREATE TABLE game_players (
+    alias TEXT,
+    room_code TEXT,
+    user_id INTEGER,
+    guest_id INTEGER,
+    FOREIGN KEY(room_code) REFERENCES game_sessions(room_code),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
 CREATE TABLE IF NOT EXISTS redherrings (
     name TEXT PRIMARY KEY
 );
